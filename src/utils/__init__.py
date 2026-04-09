457dfb0a53b007915f06957cee4388a0c6dd1f46
Gorobets Nikita <sawwa.cwer13@gmail.com>
2026-04-09 12:30:12 +0300

    feat(network): добавить поддержку IPv6 DNS и рефакторинг валидации IP-адресов

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,8 +1,11 @@
 from .subproc import run_hidden, get_system32_path, get_syswow64_path, get_system_exe
+from .validators import IPValidator, DNSValidator
 
 __all__ = [
     "run_hidden",
     "get_system32_path",
     "get_syswow64_path",
     "get_system_exe",
+    "IPValidator",
+    "DNSValidator",
 ]
